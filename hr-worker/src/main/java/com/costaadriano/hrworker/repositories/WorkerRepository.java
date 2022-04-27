36a4089a1b7f21fe431ab090172e1f20c34c4f68
Adriano Costa <costa_adriano78@hotmail.com>
2022-04-27 19:31:32 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/costaadriano/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.costaadriano.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.costaadriano.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
+}
