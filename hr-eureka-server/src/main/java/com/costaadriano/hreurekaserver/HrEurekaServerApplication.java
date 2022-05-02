ea5170b8bc8b95e4dce2a201962fe4da0b3b12de
Adriano Costa <costa_adriano78@hotmail.com>
2022-05-02 11:37:56 -0300

    Configuring Eureka clients

--- hr-eureka-server/src/main/java/com/costaadriano/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/costaadriano/hreurekaserver/HrEurekaServerApplication.java
@@ -3,7 +3,8 @@ package com.costaadriano.hreurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+import org.springframework.stereotype.Component;
+@Component
 @EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
@@ -11,5 +12,4 @@ public class HrEurekaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrEurekaServerApplication.class, args);
 	}
-
 }
